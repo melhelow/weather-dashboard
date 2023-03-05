d7718e5648050a53a30148cd882c796c8e810ffe
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-03-05 00:33:00 -0600

    initial commit

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,5 @@
-// enter city in search area
-// press search
+// user enter city in search area
+// user press search
 // display search city date and weather in weather now container
   
 // 5 days weather dsiplayed in each card
@@ -9,26 +9,28 @@ var searchInput = document.querySelector('#city-search')
 var search = document.querySelector('#search-button')
 var currentWeather = document.querySelector('#weather-today')
 var city = document.querySelector('#city-name')
-var temprature = document.querySelector('#temp')
-var humid = document.querySelector('#humidity')
+// var temprature = document.querySelector('#temp')
+// var humid = document.querySelector('#humidity')
 
-function displayCurrentWeather(cityName){
-    event.preventDefault()
+function displayCurrentWeather(){
+   
 
-    
-    var apiKey = '7f2d505fb6241cc054341ef830069eae'
-    var weatherUrl = 'api.openweathermap.org/data/2.5/forecast'
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?id=" + cityName + "&appid=" + apiKey
+
+    var apiKey = "7f2d505fb6241cc054341ef830069eae";
+    var weatherUrl = "api.openweathermap.org/data/2.5/forecast"
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
     fetch(requestUrl)
   .then(function(response) {
-      return response.json()    
+      return response.json();    
   }).then(function(weather){
-    console.log('here is weather', weather)
-
-    // for (var i = 0; i < weather.data.length; i++){
-    //     var title = weather.main[i].title
-    //     console.log('title', title)
-    // }
+    for (var i = 0; i < weather.data.length; i++){
+        var title = weather.main[i].title
+        console.log('title', title)
+    }
 })
 }
+
+  
+
+searchInput.addEventListener('click', displayCurrentWeather);
